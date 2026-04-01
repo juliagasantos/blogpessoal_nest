14125a80a2b3a3da7423171e6d10b6e1a4d70c6a
Júlia Santos <juliagalvaoalmeidasantos@gmail.com>
2026-04-01 15:09:19 -0300

    feat: implementa CRUD completo de Postagem (create, findById, findByTitulo, update, delete) e testes no Insomnia

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
@@ -6,11 +6,35 @@ import { PostagemService } from '../services/postagem.service';
 @Controller('/postagens')
 export class PostagemController {
 
-  constructor(private readonly postagemService: PostagemService) {}
+  constructor(private readonly postagemService: PostagemService) { }
 
   @Get()
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
   }
 
+  @Get('/:id')
+  findById(@Param('id') id: number) {
+    return this.postagemService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  findByTitulo(@Param('titulo') titulo: string) {
+    return this.postagemService.findByTitulo(titulo);
+  }
+
+  @Post()
+  create(@Body() postagem: Postagem) {
+    return this.postagemService.create(postagem);
+  }
+
+  @Put()
+  update(@Body() postagem: Postagem) {
+    return this.postagemService.update(postagem);
+  }
+
+  @Delete('/:id')
+delete(@Param('id') id: number) {
+  return this.postagemService.delete(id);
+}
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 
@@ -9,10 +9,53 @@ export class PostagemService {
   constructor(
     @InjectRepository(Postagem)
     private postagemRepository: Repository<Postagem> //Injeção de dependência
-  ) {}
+  ) { }
 
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find(); // select * from tb_postagem;
   }
 
+  async findById(id: number) {
+    const postagem = await this.postagemRepository.findOne({
+      where: { id }
+    });
+
+    if (!postagem) {
+      throw new NotFoundException('Postagem não encontrada!');
+    }
+
+    return postagem;
+  }
+
+  async findByTitulo(titulo: string) {
+    return await this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`)
+      }
+    });
+  }
+
+  async create(postagem: Postagem) {
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem) {
+  const buscaPostagem = await this.findById(postagem.id);
+
+  if (!buscaPostagem) {
+    throw new NotFoundException('Postagem não encontrada!');
+  }
+
+  return await this.postagemRepository.save(postagem);
+}
+
+async delete(id: number) {
+  const postagem = await this.findById(id);
+
+  if (!postagem) {
+    throw new NotFoundException('Postagem não encontrada!');
+  }
+
+  await this.postagemRepository.delete(id);
+}
 }
